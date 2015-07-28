7ff531f9662cc8b8b64bdaf406223ebba56b3d33
Leider <Leider@Leider-PC>
2015-07-28 09:40:20 -0500

    esta es una nueva actualizacion

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     </head>
     <body>
         <?php echo "hola mundo, esta es una nueva actualizacion" ?>
-        
+        <div>Este es algo nuevo</div>
+        esta es una edicion desde el sitio de 
+        Hola a todo el mundo nueva nuevamente
     </body>
 </html>
